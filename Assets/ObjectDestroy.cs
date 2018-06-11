8cf972a72920d67d8cea7b140e33604965134053
daiki-ypshimaru <godeater1104@yahoo.co.jp>
2018-06-11 21:58:32 +0900

    StepAsideUnityChan
    Lesson6で作成したゲーム及び課題の提出。

---
+++ Assets/ObjectDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+{
+    public GameObject unitychan;
+    // Use this for initialization
+    void Start()
+    {
+        unitychan = GameObject.Find("unitychan");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unitychan.transform.position.z > this.transform.position.z) { Destroy(this.gameObject); }
+    }
+}
